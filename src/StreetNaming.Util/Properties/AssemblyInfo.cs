9fe1e8a6429045a823aec3a887fe930d63bb01b7
Chris Pickford <chrispickford@gmail.com>
2016-02-24 15:39:49 +0000

    Migrated solution to ASP.NET Core structure

--- src/StreetNaming.Util/Properties/AssemblyInfo.cs
+++ src/StreetNaming.Util/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("StreetNaming.Util")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("126e745a-74b5-4161-99f4-98d4575764fb")]
+[assembly: Guid("d0b9c73a-e80a-4d21-a587-296f79fab0c7")]
