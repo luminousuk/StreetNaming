2861dc107f26bf7718608e7145d96ec858e420bb
Chris Pickford <chrispickford@gmail.com>
2016-03-15 11:09:32 +0000

    Validation

---
+++ src/StreetNaming.Util/DataAnnotations/RequiredCollectionAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace StreetNaming.Util.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class RequiredCollectionAttribute : ValidationAttribute
+    {
+        protected const string DefaultErrorMessageFormatString = "You must provide at least one";
+
+        public RequiredCollectionAttribute() : base(DefaultErrorMessageFormatString)
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return CheckValueIsValid(value)
+                ? ValidationResult.Success
+                : new ValidationResult(ErrorMessageString);
+        }
+
+        public override bool IsValid(object value)
+        {
+            return CheckValueIsValid(value);
+        }
+
+        private static bool CheckValueIsValid(object value)
+        {
+            var collection = value as ICollection;
+
+            return collection?.Count > 0;
+        }
+    }
+}
